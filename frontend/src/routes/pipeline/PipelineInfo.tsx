944bbd24e879c477eff26a3d8e12b26e15473c3c
SSR-Med <sasalazarr@unal.edu.co>
2024-05-30 09:59:03 -0500

    Fix(Changed api route)

--- frontend/src/routes/pipeline/PipelineInfo.tsx
+++ frontend/src/routes/pipeline/PipelineInfo.tsx
@@ -52,7 +52,6 @@ export default function PipelineInfo(){
                         <div className="pipeline-button">
                             <Button onClick={async () => {
                                 const currentDate = await getDate();
-                                console.log(currentDate)
                                 createPipelineRecord({
                                     reference: reference,
                                     date: currentDate,
